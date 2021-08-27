722bc79c85e4da27998298f63d875385ef9af6dd
Esteban A. Sanabria V <estebanasv31@gmail.com>
2021-08-26 20:02:00 -0600

    modify top to executre new testbenches

--- verification/unit_testing/top.sv
+++ verification/unit_testing/top.sv
@@ -1,6 +1,13 @@
 module top #(parameter WIDTH = 32)
 			   ();
 
-adderTestBench #(WIDTH,"outputs/add_output.txt", "inputs/add_input.txt") adderTest ();
+// simple adder testbench
+adderTestBench #(WIDTH, "outputs/add_output.txt", "inputs/add_input.txt") adderTest ();
+
+// adder with carry testbench
+adderCarryTestBench #(WIDTH, "outputs/addCarry_output.txt", "inputs/addCarry_input.txt") adderCarryTest ();
+
+//alu testbench
+aluTestBench #(WIDTH, "outputs/alu_output.txt", "inputs/alu_input.txt") aluTest ();
 
 endmodule
