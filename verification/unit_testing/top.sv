45ca7f57609e0315cc3ffb2892e682541576f2a3
Esteban A. Sanabria V <estebanasv31@gmail.com>
2021-08-27 19:37:50 -0600

    ADD mux testbench

--- verification/unit_testing/top.sv
+++ verification/unit_testing/top.sv
@@ -13,4 +13,10 @@ aluTestBench #(WIDTH, "outputs/alu_output.txt", "inputs/alu_input.txt") aluTest 
 //conditional logic testbench
 condcheckTestBench #(WIDTH, "outputs/condCheck_output.txt", "inputs/condCheck_input.txt") condCheckTest ();
 
+//condlogicTestBench #(WIDTH, "outputs/condLogic_output.txt", "inputs/condLogic_input.txt") condLogicTest ();
+
+//mux testbench
+muxTestBench #(WIDTH, "outputs/mux_output.txt", "inputs/mux_input.txt") muxTest ();
+
+
 endmodule
