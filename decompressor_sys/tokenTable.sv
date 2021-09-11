6e5584802e56451dbc504984bcb3c723d6ac4d07
esteasv31 <estebanasv31@gmail.com>
2021-09-11 11:23:21 -0600

    add internal TOKEN TABLE mem

---
+++ decompressor_sys/tokenTable.sv
@@ -0,0 +1,19 @@
+module tokenTable #(parameter WIDTH = 32,
+						  parameter SIZE = 64,
+						  parameter FILE = "memfile.dat")
+						 (input logic [WIDTH-1:0] a, b, data
+						  input logic clk, en, 
+						 output logic [WIDTH-1:0] rd1, rd2);
+				
+logic [WIDTH-1:0] ROM[SIZE-1:0];
+
+initial
+	$readmemh(FILE, ROM);
+	
+always_ff @(posedge clk)
+	if (en) ROM[a[WIDTH-1:2]] = data;
+
+assign rd1 = ROM[a[WIDTH-1:2]]; // word aligned
+assign rd2 = ROM[b[WIDTH-1:2]]; // word aligned
+
+endmodule
