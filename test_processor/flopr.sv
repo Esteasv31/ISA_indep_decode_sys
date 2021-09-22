c065634d5bae28da01108435abf6c32e98edcc4d
Esteban A. Sanabria V <estebanasv31@gmail.com>
2021-09-22 17:10:11 -0600

    ADD custom initial value to flip-flop-r

--- test_processor/flopr.sv
+++ test_processor/flopr.sv
@@ -1,10 +1,10 @@
-module FLOPR #(parameter WIDTH = 32)
+module FLOPR #(parameter WIDTH = 32, parameter INITVALUE = 0)
 				  (input logic clk, reset, en,
 				   input logic [WIDTH-1:0] d,
 				  output logic [WIDTH-1:0] q);
 				  
 always_ff @(posedge clk, posedge reset)
-	if (reset) q <= 0;
+	if (reset) q <= INITVALUE;
 	else if (en) q <= q;
 	else q <= d;
 	
